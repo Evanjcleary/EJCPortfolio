46307c0dd2cf4e61afaa3096ffb9d293d44dbda0
Evan Desktop <evanjcleary@gmail.com>
2019-09-26 18:03:29 -0400

    heroku-fix-test2b

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,27 +5,36 @@ var path = require("path");
 module.exports = function (app) {
 
     //Route to Home Page
-    app.get("/", function(req, res) {
-        
+    app.get("/", function (req, res) {
+
         var featuredJobsArray = [];
+        var featuredRequestsArray = [];
 
         homeObject = {
-            featuredJobs: featuredJobsArray
+            featuredJobs: featuredJobsArray,
+            featuredRequests: featuredRequestsArray
         }
 
-        db.Project.findAll({}).then(function(data){
+        db.Project.findAll({}).then(function (data) {
             for (i = 0; i < data.length; i++) {
                 featuredJobsArray.push(data[i].dataValues);
             }
-
+            db.Request.findAll({}).then(function (data) {
+                for (j = 0; j < data.length; j++) {
+                    console.log(data[j].dataValues);
+                    featuredRequestsArray.push(data[j].dataValues);
+                }
+            })
         })
-        
+
+
+
         res.render("index", homeObject);
 
-      });
+    });
 
 
-    app.get("/test", function(req, res) {
+    app.get("/test", function (req, res) {
         res.render("test");
     });
 
